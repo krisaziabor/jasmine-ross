cd6ee8b149e3a1f3d0b811500e6751e1c5578295
kris aziabor <krisaaziabor@gmail.com>
2025-05-07 08:25:01 -0400

    transferred new image from 35mm to 4x5

--- web/src/app/(content)/bw/35mm/page.tsx
+++ web/src/app/(content)/bw/35mm/page.tsx
@@ -6,7 +6,7 @@ export default function BWFilm35mm() {
   return (
     <PhotoSeries
       title="35mm"
-      imageCount={6} // Update with actual count when images are available
+      imageCount={5} // Update with actual count when images are available
       basePath="/35mm"
       imagePrefix="JR-35-0"
     />
